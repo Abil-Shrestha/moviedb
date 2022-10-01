55fd9c29aa4fff2ebc7f4f933b4f2c5256cfb0b2
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2022-09-30 22:54:28 -0400

    Setup

---
+++ src/components/Actors/Actors.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+const Actors = () => {
+  console.log('Actors');
+  return (
+    <div>
+      Actors
+    </div>
+  );
+};
+
+export default Actors;
